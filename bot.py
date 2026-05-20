8f346e444340a1e40d19865f38097ce4bad0809a
89532425259denis-alt <89532425259denis@gmail.com>
2026-05-20 13:13:34 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+import telebot
+import urllib.parse
+
+bot = telebot.TeleBot("8492764278:AAFDuKe2e5T6GKI4NvYJorOiameDfpSjDUM")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Отправь описание картинки!")
+
+@bot.message_handler(func=lambda message: True)
+def generate(message):
+    prompt = urllib.parse.quote(message.text)
+    url = f"https://image.pollinations.ai/prompt/{prompt}"
+    bot.send_photo(message.chat.id, url, caption="Вот ваше изображение!")
+    if __name__ == "__main__":
+    bot.infinity_polling()
